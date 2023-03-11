25700f2db04a918ff0782245ce9474a000342dbe
R-ONE <114864579+R-ONE-FR@users.noreply.github.com>
2023-03-11 08:26:20 +0100

    Update Pass_gen.py

--- Pass_gen.py
+++ Pass_gen.py
@@ -4,20 +4,18 @@ from random import choice
 
 charset = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN123456789!$%()+*-.=[]^"
 password = ''
-file_name = 'Password_manager.txt'
 
 def main():
     print('\nPython Pass_gen.py [website name] [username] [number of characters]\n\nDescription :\n\n    Generate a random password for a website\n')
 
 if len(sys.argv) == 4:
+	file_name = str(sys.argv[1])
 	print("\nSecure password :\n")
-	#print(i,end="")for i in range(int(sys.argv[1]))
 	for i in range(int(sys.argv[3])):
 		password += str(choice(charset))
 	print(password)
-	os.system('type null > Password_manager.txt')
-	os.system('echo Website : ' + str(sys.argv[1]) + ' Username : ' + str(sys.argv[2]) + ' Password : ' + password + ' > ' + file_name)
-	os.system(file_name)
+	os.system(f'type null > {file_name}.txt')
+	os.system(f'echo Website : {file_name} Username : {str(sys.argv[2])} Password : {password}> {file_name}.txt')
+	os.system(f'{file_name}.txt')
 else:
 	main()
-
